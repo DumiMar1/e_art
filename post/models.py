dd5f71fd67d0e6304c71d759029570eeef84b9de
Dumitru Marin <marindidi96@gmail.com>
2022-11-26 16:00:54 +0200

    medata read

--- post/models.py
+++ post/models.py
@@ -6,6 +6,8 @@ from django.utils import timezone
 from django.db import models
 import os
 from django.dispatch import receiver
+from exiffield.fields import ExifField
+from exiffield.getters import exifgetter
 # Create your models here.
 User = get_user_model()
 
@@ -17,6 +19,28 @@ class Post(models.Model):
     image = models.ImageField(default=None, upload_to='users_posts')
     description = models.TextField(max_length=1000)
     date_publication = models.DateTimeField(auto_now_add=True)
+    camera = models.TextField(max_length=256)
+    model= models.TextField(max_length=256)
+    aperture = models.FloatField(max_length=256)
+    shutter_speed = models.FloatField(max_length=256)
+    focal_length = models.FloatField(max_length=256)
+    iso = models.IntegerField()
+
+    exif = ExifField(
+
+        source='image',
+        denormalized_fields={
+            'camera': exifgetter('Make'),
+            'mode':exifgetter('Model'),
+            'aperture':exifgetter('Aperture'),
+            'shutter_speed':exifgetter('ShutterSpeedValue'),
+            'focal_length':exifgetter('FNumber'),
+            'iso':exifgetter('ISOSpeedRatings'),
+        },
+    )
+
+
+
 
     def __str__(self):
         return self.title
@@ -37,12 +61,3 @@ def delete_file(sender, instance, *args, **kwargs):
     if instance.image:
         _delete_file(instance.image.path)
 
-
-class PostMeta(models.Model):
-    post = models.OneToOneField(Post, on_delete=models.CASCADE)
-    camera = models.TextField(max_length=256)
-    model= models.TextField(max_length=256)
-    aperture = models.FloatField(max_length=256)
-    shutter_speed = models.FloatField(max_length=256)
-    focal_length = models.FloatField(max_length=256)
-    iso = models.IntegerField()
